acab69061c717c8b3da0f8c40ffe13bd1d32aa8d
H.M Sadiq <hmsadikurrahman120@gmail.com>
2023-06-11 22:08:30 +0600

    creat the delete class api

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 // Middelware
@@ -59,6 +59,13 @@ async function run() {
             const query ={email:email}
             const result = await classCollection.find(query).toArray();
             res.send(result);
+        })
+
+        app.delete('/class/:id',async(req,res)=>{
+            const id = req.params.id
+            const query= {_id:new ObjectId(id)}
+            const result = await classCollection.deleteOne(query);
+            res.send(result)
         })
 
         // Review Collection
